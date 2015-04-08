41e17706e7cbeb852f09405b8a8a28b8a5bdcee4
Filip Vinkovic <vinkovicf@gmail.com>
2015-04-08 23:07:41 +0200

    Minor change

--- app/src/main/java/com/filipvinkovic/MaterialAnimationsDemo/DetailActivity.java
+++ app/src/main/java/com/filipvinkovic/MaterialAnimationsDemo/DetailActivity.java
@@ -7,8 +7,6 @@ import android.transition.Slide;
 import android.transition.Transition;
 import android.transition.TransitionSet;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewTreeObserver;
@@ -112,25 +110,5 @@ public class DetailActivity extends BaseActivity {
 
     @Override protected int getLayoutResource() {
         return R.layout.activity_detail;
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
